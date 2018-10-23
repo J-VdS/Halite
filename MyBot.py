e5b1fa4b40d9a3f40c492e42055c30714e1ac918
J-VdS <38380578+J-VdS@users.noreply.github.com>
2018-10-23 19:41:44 +0200

    best one

--- MyBot.py
+++ MyBot.py
@@ -13,12 +13,13 @@ import logging
 # This game object contains the initial game state.
 game = hlt.Game()
 
-gh = {}
+load = []
+gh = []
 
 # At this point "game" variable is populated with initial map data.
 # This is a good place to do computationally expensive start-up pre-processing.
 # As soon as you call "ready" function below, the 2 second per turn timer will start.
-game.ready("Testbot v6")
+game.ready("Testbot 500")
 
 # Now that your bot is initialized, save a message to yourself in the log file with some important information.
 #   Here, you log here your id, which you can always fetch from the game object by using my_id.
@@ -32,6 +33,7 @@ while True:
     me = game.me
     if game.turn_number == 1:
         shipyard = me.shipyard.position
+        
         '''logging.info(str(me.shipyard))
         logging.info(type(me.shipyard))
         try:
@@ -40,55 +42,44 @@ while True:
         except:
             pass'''
     game_map = game.game_map
-
     command_queue = []
-    move_queue = []
-
-    for ship in me.get_ships():            
-        if ship.position == shipyard:
-            gh[ship.id] = False          
-            com, move_queue = functions.max_halite(game_map, ship, move_queue)
-            command_queue.append(com)
-        
-        elif gh[ship.id] or ship.halite_amount >700:
-            move = game_map.naive_navigate(ship, shipyard)
-            if move != Direction.Still:
-                pos = Position(move[0], move[1])+ship.position
-                if pos in move_queue:
-                    move = Direction.Still
-                else:
-                    move_queue.append(pos)
-            command_queue.append(ship.move(move))
-                
-            
-            #game_map.naive_navigate(ship, shipyard)))
-            gh[ship.id] = True
-            
-        
-        elif game_map[ship.position].halite_amount >= constants.MAX_HALITE / 10:
-            command_queue.append(ship.stay_still())
     
-        else:
-            com, move_queue = functions.max_halite(game_map, ship, move_queue)
-            command_queue.append(com)
-        
-        '''
-        if ship.halite_amount>400:
-            command_queue.append(
-                ship.move(
-                    game_map.naive_navigate(ship, shipyard)))
-        
-        
-        elif game_map[ship.position].halite_amount < constants.MAX_HALITE / 10:
-            command_queue.append(
-                ship.move(
-                    random.choice([ Direction.North, Direction.South, Direction.East, Direction.West ])))
-        else:
+    logging.info(load)
+    
+    for ship in me.get_ships():
+        if ship.id in load:
             command_queue.append(ship.stay_still())
-        '''
-    if len(me.get_ships())<10 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
+            if not game_map[ship.position].halite_amount >= constants.MAX_HALITE / 10:
+                load.remove(ship.id)
+            elif ship.halite_amount > 900:
+                gh.append(ship.id)
+                load.remove(ship.id)
+        elif ship.halite_amount > 700 and ship.id not in gh:
+            gh.append(ship.id)
+            move = game_map.naive_navigate(ship, shipyard)
+            command_queue.append(ship.move(move))
+            if Position(move[0], move[1]) + ship.position == shipyard:
+                gh.remove(ship.id)  
+        elif ship.id in gh:
+            move = game_map.naive_navigate(ship, shipyard)
+            command_queue.append(ship.move(move))
+            if Position(move[0], move[1]) + ship.position == shipyard:
+                gh.remove(ship.id)
+
+        else:
+            command_queue.append(functions.max_halite2(game_map, ship))
+            load.append(ship.id)
+            logging.info(load)
+    
+    
+    
+    
+    
+    if game.turn_number <=200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
         command_queue.append(me.shipyard.spawn())
+        
 
     # Send your moves back to the game environment, ending this turn.
     game.end_turn(command_queue)
+    
 
